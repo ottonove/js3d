8a0a1ca0f46468924e401945a56fa26fb4cb9a88
tomokazu-infomorph <tomokazu.ogawa@infomorph.jp>
2021-03-06 20:24:42 +0900

    横の動きも加えた。

--- wave.js
+++ wave.js
@@ -15,12 +15,12 @@ const animate = () => {
 
   A += 0.05;
 
-  const x = 80; // 振幅
-  
+  const x = 100; // 振幅
+  const s = Math.PI/2;
   //円
   ctx.fillStyle = 'rgba(255,255,255)';
   ctx.beginPath();
-  ctx.arc(width, Math.sin(A)*x+height, Math.sin(A+1.3)*20+40, 0, Math.PI*2, false);
+  ctx.arc(Math.cos(A)*-50+width, Math.sin(A)*x+height, Math.sin(s+A)*20+40, 0, Math.PI*2, false);
   ctx.fill();
 
   console.log('animate');
