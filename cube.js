57009b675db6da14a49ff1a9993f97c4eaf9af98
tomokazu-infomorph <tomokazu.ogawa@infomorph.jp>
2021-03-07 13:08:55 +0900

    cubeクラスを追加。

--- cube.js
+++ cube.js
@@ -5,7 +5,41 @@ class Vertex {
     this.z = parseFloat(z);
   }
 };
+// const A = new Vertex(10, 20, 0.5);
+// console.log('A.x: ',A.x);
 
-const A = new Vertex(10, 20, 0.5);
+/**
+ * 立方体クラス          : Cube(new Vertex(0, 0, 0), 200)
+ * new Vertex(0, 0, 0)  : 中心座標 x, y, z
+ * 200                  : 1辺の長さ
+ */
+class Cube {
+  constructor(center, size){
+    // Generate the vertices
+    const d = size / 2;
 
-console.log('A.x: ',A.x);
+    this.vertices = [
+        new Vertex(center.x - d, center.y - d, center.z + d),
+        new Vertex(center.x - d, center.y - d, center.z - d),
+        new Vertex(center.x + d, center.y - d, center.z - d),
+        new Vertex(center.x + d, center.y - d, center.z + d),
+        new Vertex(center.x + d, center.y + d, center.z + d),
+        new Vertex(center.x + d, center.y + d, center.z - d),
+        new Vertex(center.x - d, center.y + d, center.z - d),
+        new Vertex(center.x - d, center.y + d, center.z + d)
+    ];
+
+    // Generate the faces
+    this.faces = [
+      [this.vertices[0], this.vertices[1], this.vertices[2], this.vertices[3]],
+      [this.vertices[3], this.vertices[2], this.vertices[5], this.vertices[4]],
+      [this.vertices[4], this.vertices[5], this.vertices[6], this.vertices[7]],
+      [this.vertices[7], this.vertices[6], this.vertices[1], this.vertices[0]],
+      [this.vertices[7], this.vertices[0], this.vertices[3], this.vertices[4]],
+      [this.vertices[1], this.vertices[6], this.vertices[5], this.vertices[2]]
+    ];
+  }
+};
+
+const cube = new Cube(new Vertex(0, 0, 0), 200);
+console.log('cube:', cube);
